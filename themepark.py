48c6b6d31d692f8e71d598a4fe6baf9cdd917273
Rebecca <rebeccadavidsson3@gmail.com>
2020-01-10 14:18:32 +0100

    Vizualisatie framework

---
+++ themepark.py
@@ -0,0 +1,20 @@
+from PersonModel import *
+from mesa.visualization.modules import CanvasGrid
+from mesa.visualization.ModularVisualization import ModularServer
+
+
+def agent_portrayal(agent):
+    portrayal = {"Shape": "circle",
+                 "Filled": "true",
+                 "Layer": 0,
+                 "Color": "red",
+                 "r": 0.5}
+    return portrayal
+
+grid = CanvasGrid(agent_portrayal, 10, 10, 500, 500)
+server = ModularServer(PersonModel,
+                       [grid],
+                       "Theme park model",
+                       {"N":100})
+server.port = 8521 # The default
+server.launch()
