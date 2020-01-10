418b3c7722e530d903a1a9eb1f6bc069819ed914
samverh <43989279+samverh@users.noreply.github.com>
2020-01-10 23:38:17 +0100

    begin to add customers at random locations, customers are added to grid but not visible yet, TODO: fix customer portrayal

--- models/model.py
+++ models/model.py
@@ -2,58 +2,104 @@ from mesa import Model, Agent
 from mesa.space import SingleGrid
 from mesa.space import MultiGrid
 from mesa.time import RandomActivation
+from mesa.datacollection import DataCollector
 from collections import Counter
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from matplotlib import style
+import matplotlib.pylab as plt
 import random
+import numpy as np
 
-x_list = [1, 13, 25]
+x_list = [0, 13, 25]
 y_list = [13, 25, 13]
-positions = [(1,13), (13,25), (25, 13)]
+positions = [(0,13), (13,25), (25, 13)]
 num_people = 100
 heading = (1, 0)
 
 
-class Custumor(Agent):
+class Customer(Agent):
     def __init__(self, unique_id, model, pos, heading=(1, 0)):
         super().__init__(unique_id, model)
         self.pos = pos
+        self.model = model
         self.heading = heading
         self.headings = {(1, 0), (0, 1), (-1, 0), (0, -1)}
+
+    def random_move(self):
+        neigh_cells = self.model.grid.get_neighborhood(self.pos, True)
+        # print("neighborhood: ", neigh_cells)
+        selected_cell = neigh_cells[0]
+        self.model.grid.move_agent(self, selected_cell)
+
+    def step(self):
+        # The agent's step will go here.
+        # self.random_move()
+        pass
 
 
 class Attraction(Agent):
     def __init__(self, unique_id, model, pos, heading=(1, 0)):
         super().__init__(unique_id, model)
         self.pos = pos
+        self.model = model
         self.heading = heading
         self.headings = {(1, 0), (0, 1), (-1, 0), (0, -1)}
 
+    def step(self):
+        # The agent's step will go here.
+        pass
+
 
 class Themepark(Model):
-    def __init__(self, N=2, width=20, height=10):
-        self.N = N    # num of agents
+    def __init__(self, N_attr, N_cust, width, height):
+        self.N_attr = N_attr    # num of attraction agents
+        self.N_cust = N_cust    # num of customer agents
+        self.width = width
+        self.height = height
+
         self.headings = ((1, 0), (0, 1), (-1, 0), (0, -1))
+
         self.grid = MultiGrid(width, height, torus=False)
-        self.schedule = RandomActivation(self)
+
+        # Dit onderscheid is waarschijnlijk niet nodig, maar werd zo gedaan in wolf/sheep dus even testen
+        self.schedule_Customer = RandomActivation(self)
+        self.schedule_Attraction = RandomActivation(self)
+
+        self.datacollector = DataCollector(
+            {"Customer": lambda m: self.schedule_Customer.get_agent_count(),
+            "Attraction": lambda m: self.schedule_Attraction.get_agent_count()})
+
         self.make_attractions()
+        self.add_customers()
+        # self.add_people()
+
         self.running = True
-        self.add_people()
+        self.datacollector.collect(self)
 
     def make_attractions(self):
         """ Initialize attractions on fixed position."""
 
-        for i in range(self.N):
-
+        for i in range(self.N_attr):
             pos = (x_list[i], y_list[i])
 
             if self.grid.is_cell_empty(pos):
-                print("Creating agent {2} at ({0}, {1})"
+                print("Creating ATTRACTION agent {2} at ({0}, {1})"
                       .format(x_list[i], y_list[i], i))
                 a = Attraction(i, self, pos, heading)
-                self.schedule.add(a)
+                self.schedule_Attraction.add(a)
                 self.grid.place_agent(a, pos)
+
+    def add_customers(self):
+        """ Initialize customers on random positions."""
+
+        for i in range(self.N_cust):
+            rand_x = random.choice(list(np.arange(0, self.width)))
+            rand_y = random.choice(list(np.arange(0, self.height)))
+            pos = (rand_x, rand_y)
+
+            print("Creating CUSTOMER agent {2} at ({0}, {1})"
+                  .format(rand_x, rand_y, i))
+            a = Customer(i, self, pos, heading)
+            self.schedule_Customer.add(a)
+            self.grid.place_agent(a, pos)
 
     def add_people(self):
         """Add people to a random attraction."""
@@ -66,8 +112,8 @@ class Themepark(Model):
             position_counter.append(pos)
 
             # Make individual agents
-            custumor = Custumor(person, self, pos, heading)
-            self.grid.place_agent(custumor, pos)
+            customer = Customer(person, self, pos, heading)
+            self.grid.place_agent(customer, pos)
 
         # Calculate how many customers are where
         count = [[x, position_counter.count(x)] for x in set(position_counter)]
@@ -76,8 +122,16 @@ class Themepark(Model):
         for i in count:
             x.append(i[1])
 
-        y_pos = ['Attraction1', 'Attraction2', 'Attraction3']
+        y_pos = ['Attraction1','Attraction2','Attraction3']
         plt.bar(y_pos, x, align='center', alpha=0.5)
-        plt.xticks([0, 1, 2], y_pos)
+        plt.xticks([0,1,2], y_pos)
 
-        # plt.show()
+        plt.show()
+
+    def step(self):
+        """Advance the model by one step."""
+        # self.schedule_Customer.step()
+        # self.schedule_Attraction.step()
+        #
+        # self.datacollector.collect(self)
+        pass
--- models/server.py
+++ models/server.py
@@ -1,7 +1,8 @@
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 
-from .model import Attraction, Themepark
+from .model import Customer, Attraction, Themepark
+from mesa.visualization.modules import ChartModule
 
 
 def agent_draw(agent):
@@ -9,7 +10,7 @@ def agent_draw(agent):
     if agent is None:
         pass
     elif isinstance(agent, Attraction):
-        print("Uid: {0}, Heading: {1}".format(agent.unique_id, agent.heading))
+        print("ATTRACTION Uid: {0}, Heading: {1}".format(agent.unique_id, agent.heading))
         portrayal = {
             "Shape": "arrowHead",
             "Filled": "true",
@@ -23,19 +24,45 @@ def agent_draw(agent):
             "text_color": "white",
             "scale": 0.8,
         }
+
+    # HIER GAAT HET MIS - hoe verschillende types agents een verschillende portrayal geven?
+
+    # elif isinstance(agent, Customer):
+    #     portrayal = None
+    #     print("CUSTOMER Uid: {0}, Heading: {1}".format(agent.unique_id, agent.heading))
+    #     portrayal = {
+    #         "Shape": "circle",
+    #         "Filled": "true",
+    #         "Layer": 1,
+    #         "Color": ["#FFFFFF", "#99FF99"],
+    #         "stroke_color": "#666666",
+    #         "Filled": "true",
+    #         "heading_x": agent.heading[0],
+    #         "heading_y": agent.heading[1],
+    #         "text": agent.unique_id,
+    #         "text_color": "white",
+    #         "scale": 0.8,
+    #     }
+
     return portrayal
 
 
 width = 26
 height = 26
-num_agents = 3
+N_attr = 3
+N_cust = 5
 pixel_ratio = 26
 grid = CanvasGrid(agent_draw, width, height, width * pixel_ratio, height * pixel_ratio)
-server = ModularServer(
-    Themepark,
-    [grid],
-    "Theme Park Model",
-    {"N": num_agents, "width": width, "height": height},
-)
-server.max_steps = 0
+
+chart = ChartModule([{"Label": "Barchart",
+                      "Color": "Black"}],
+                    data_collector_name='datacollector')
+
+server = ModularServer(Themepark,
+                       # [grid, chart],
+                       [grid],
+                       "Theme Park Model",
+                       {"N_attr": N_attr, "N_cust": N_cust, "width": width, "height": height})
+
+# server.max_steps = 0
 server.port = 8521
